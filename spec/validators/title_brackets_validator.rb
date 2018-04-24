e8fd50da8d7ff2c17fe31a8f6143680c988fc279
nejcik <nataliaxskowronek@gmail.com>
2018-04-24 12:19:11 +0200

    Add validation titles with not enough brackets

--- spec/validators/title_brackets_validator.rb
+++ spec/validators/title_brackets_validator.rb
@@ -1,4 +1,24 @@
 class TitleBracketsValidator < ActiveModel::EachValidator
-  def validate(document)
+
+  def validate_each(record, attribute, value)
+    special = "()[]}{)({}"
+    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/ # to find any of those
+    if value =~ regex  # if ANY of those are in this string 
+      basic_open_bracket = (0 ... value.length).find_all { |i| value[i,1] == '(' }
+      basic_close_bracket = (0 ... value.length).find_all { |i| value[i,1] == ')' }
+      square_open_bracket = (0 ... value.length).find_all { |i| value[i,1] == '[' }
+      square_close_bracket = (0 ... value.length).find_all { |i| value[i,1] == ']' }
+      curly_open_bracket = (0 ... value.length).find_all { |i| value[i,1] == '{' }
+      curly_close_bracket = (0 ... value.length).find_all { |i| value[i,1] == '}' }
+
+      # if the length of compared array bracket is not fine -> there is not enough brackets! 
+      if basic_open_bracket.length != basic_close_bracket.length || square_open_bracket.length != square_close_bracket.length || curly_open_bracket.length != curly_close_bracket.length
+        record.errors.add attribute, (options[:message] || "Not enough brackets (opened & not closed or closed without opening)")
+      end
+      
+      # check if the order is OK
+    end
+    # has no special char => title is fine
   end
+  
 end
