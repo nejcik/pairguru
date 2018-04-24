72e39f4f201f3efe7a22d9156ba176b60167d467
nejcik <nataliaxskowronek@gmail.com>
2018-04-24 12:45:17 +0200

    Validation for empty brackets

--- spec/validators/title_brackets_validator.rb
+++ spec/validators/title_brackets_validator.rb
@@ -16,19 +16,21 @@ class TitleBracketsValidator < ActiveModel::EachValidator
         record.errors.add attribute, (options[:message] || "Not enough brackets (opened & not closed or closed without opening)")
       else 
         # check if the order is OK
-        order(record, attribute, basic_open_bracket, basic_close_bracket)
-        order(record, attribute, square_open_bracket, square_close_bracket)
-        order(record, attribute, curly_open_bracket, curly_close_bracket)
+        order_or_empty(record, attribute, basic_open_bracket, basic_close_bracket)
+        order_or_empty(record, attribute, square_open_bracket, square_close_bracket)
+        order_or_empty(record, attribute, curly_open_bracket, curly_close_bracket)
       end
     end
     # has no special char => title is fine
   end
   
-  def order(record, attribute, open_brackets, close_brackets)
+  def order_or_empty(record, attribute, open_brackets, close_brackets)
     if !open_brackets.empty?
       (0...open_brackets.length).each do |i|
         if open_brackets[i] > close_brackets[i]
           record.errors.add attribute, (options[:message] || "Wrong order!")
+        elsif close_brackets[i] - open_brackets[i] == 1
+          record.errors.add attribute, (options[:message] || "Can't be empty!")
         end
       end
     end
