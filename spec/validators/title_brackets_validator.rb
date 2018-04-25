7329fd4d9735819398362d4fdadf4b858bc9c0a3
nejcik <nataliaxskowronek@gmail.com>
2018-04-25 21:45:28 +0200

    deleted commnted out code

--- spec/validators/title_brackets_validator.rb
+++ spec/validators/title_brackets_validator.rb
@@ -1,9 +1,6 @@
 class TitleBracketsValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    #special = "()[]}{)({}"
-    #regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/ # to find any of those
-    #if value =~ regex  # if ANY of those are in this string 
       basic_open_bracket = (0 ... value.length).find_all { |i| value[i,1] == '(' }
       basic_close_bracket = (0 ... value.length).find_all { |i| value[i,1] == ')' }
       square_open_bracket = (0 ... value.length).find_all { |i| value[i,1] == '[' }
@@ -20,8 +17,6 @@ class TitleBracketsValidator < ActiveModel::EachValidator
         order_or_empty(record, attribute, square_open_bracket, square_close_bracket)
         order_or_empty(record, attribute, curly_open_bracket, curly_close_bracket)
       end
-    #end
-    # has no special char => title is fine
   end
   
   def order_or_empty(record, attribute, open_brackets, close_brackets)
